0c3f4af74385822da798ce6c33f5588c2d5edc69
huiliaoliao <31496673+huiliaoliao@users.noreply.github.com>
2018-01-16 16:38:33 +0800

    pull

--- src/pages/buyLottery/children/betComponents/bet_result.vue
+++ src/pages/buyLottery/children/betComponents/bet_result.vue
@@ -365,15 +365,15 @@
             .then(res => {
               //console.log(res.data)
               if (res.data.status == 200) {
-                this.$http.post("/member/getBlance", { memberId: memberId })
-                  .then(res => {
-                    if (res.data.status == 200) {
-                      console.log(res)
+//                this.$http.post("/member/getBlance", { memberId: memberId })
+//                  .then(res => {
+//                    if (res.data.status == 200) {
+//                      console.log(res)
 //                      let userInfo = JSON.parse(localStorage.getItem('user'))
 //                      userInfo.balance = res.data.balance
 //                      localStorage.setItem('user', JSON.stringify(userInfo))
-                    }
-                  }).catch(res => console.log(res));
+//                    }
+//                  }).catch(res => console.log(res));
                 this.$alert('投注成功', '温馨提示', {
                   confirmButtonText: '确定',
                   center: true,
